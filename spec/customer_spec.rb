6647563a3997f71702d3162f9932c621dd7bb04a
Max MacGillivray <mary.macgillivray@gmail.com>
2024-01-17 14:45:16 -0700

    Created testing for #adopt, created adopt method.

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,10 +1,14 @@
 require 'pry'
+require './lib/pet'
 require './lib/customer'
 
 RSpec.describe Customer do
   before :each do
     @joel = Customer.new("Joel", 2)
     # @joel = Customer.new({name: "Joel", id: 2})
+    
+    @samson = Pet.new({name: "Samson", type: :dog, age: 3})
+    @lucy = Pet.new({name: "Lucy", type: :cat, age: 12})
   end
   
   describe '#initialize' do
@@ -18,5 +22,19 @@ RSpec.describe Customer do
       expect(@joel.pets).to eq([])
     end
   end
+  
+  describe '#adopt' do
+    it 'adds pets to a customer' do
+      expect(@joel.pets).to eq([])
+      expect(@samson).to be_a(Pet)
+      expect(@lucy).to be_a(Pet)
+      
+      @joel.adopt(@samson)
+      @joel.adopt(@lucy)
+      
+      expect(@joel.pets).to eq([@samson, @lucy])
+    end
+  end
+  
 end
 
