376ec600bfff525f5112a146a8ec6d46c929ec97
Max MacGillivray <mary.macgillivray@gmail.com>
2024-01-23 17:20:33 -0700

    Struggled to make the all_pets method, but it works

--- lib/day_care.rb
+++ lib/day_care.rb
@@ -21,7 +21,25 @@ class DayCare
   end
   
   def all_pets
+    @customers.flat_map(&:pets)
     
+    # @customers.find_all { |customer| customer.find_all {|pets| } pets }
+    
+    # @customer.find_all do customer
+    #   customer.pets.map do pet
+    #     [] << pet
+    #   end
+    # end
+    # @customers.find_all { |customer| customer.outstanding_balance > 0 }
+    # binding.pry
+    # binding.pry
+    # @customers.each do
+    #   binding.pry
+    #   customer.pets.map do
+    #     [] << pet
+    #   end
+    # end
+    # binding.pry
   end
   
   def hungry_pets
